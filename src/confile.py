7ee22f4208dc75791678bee597138a4489d158c3
Lianming Du <adullb@qq.com>
2025-09-23 23:14:35 +0800

    Update confile.py

--- src/confile.py
+++ src/confile.py
@@ -32,82 +32,110 @@ class CirchartCircosConfile(Confile):
 		self.params = params
 		self.parse()
 
+	def parse_ideogram(self, ps):
+		with Tag('ideogram'):
+			for k, v in ps.items():
+				match k:
+					case 'spacing':
+						with Tag('spacing'):
+							self.option('default', v, 'r')
+
+					case 'radius':
+						self.option(k, v, 'r')
+
+					case 'thickness':
+						self.option(k, v, 'p')
+
+					case _:
+						self.option(k, v)
+
+	def parse_track(self, tracks, name='plot'):
+		with Tag('{}s'.format(name)):
+			for track in tracks:
+				with Tag(name):
+					for k, v in track.items():
+						match k:
+							case 'data':
+								self.option('file', 'data{}.txt'.format(v))
+
+							case 'type':
+								if v == 'highlight':
+									pass
+
+								else:
+									self.option(k, v)
+
+							case 'r0' | 'r1':
+								if track['type'] == 'highlight' and track['ideogram'] == 'yes':
+									pass
+
+								else:
+									self.option(k, v, 'r')
+
+							case 'color':
+								if isinstance(v, list):
+									cs = []
+
+									for c in v:
+										if c not in custom_colors:
+											custom_colors.append(c)
+
+										cid = custom_colors.index(c)
+										cs.append('cc{}'.format(cid))
+
+									self.option(k, ','.join(cs))
+
+								else:
+									if v not in custom_colors:
+										custom_colors.append(v)
+
+									cid = custom_colors.index(v)
+									self.option(k, 'cc{}'.format(cid))
+
+							case _:
+								self.option(k, v)
+
 	def parse(self):
 		Confile._blocks = []
-		option = self.option
-		include = self.include
-		tag = Tag
 
 		#karyotype
 		kfiles = ['karyotype{}.txt'.format(i) \
 			for i in self.params['karyotype']]
-		option('karyotype', ','.join(kfiles))
+		self.option('karyotype', ','.join(kfiles))
 
 		#ideogram
 		ps = self.params['ideogram']
-		with tag('ideogram'):
-			for k, v in ps.items():
-				match k:
-					case 'spacing':
-						with tag('spacing'):
-							option('default', v, 'r')
-
-					case 'radius':
-						option(k, v, 'r')
-
-					case 'thickness':
-						option(k, v, 'p')
-
-					case _:
-						option(k, v)
+		self.parse_ideogram(ps)
+		
 
 		#tracks
 		custom_colors = []
-		if any([p.startswith('track') for p in self.params]):
-			with tag('plots'):
-				for p in self.params:
-					if p.startswith('track'):
-						ps = self.params[p]
+		plot_tracks = []
+		highlight_tracks = []
 
-						with tag('plot'):
-							for k, v in ps.items():
-								match k:
-									case 'data':
-										option('file', 'data{}.txt'.format(v))
+		for p in self.params:
+			if p.startswith('track'):
+				ps = self.params[p]
 
-									case 'r0' | 'r1':
-										option(k, v, 'r')
+				if ps['type'] == 'highlight':
+					highlight_tracks.append(ps)
+				else:
+					plot_tracks.append(ps)
 
-									case 'color':
-										if isinstance(v, list):
-											cs = []
+		if plot_tracks:
+			self.parse_track(plot_tracks)
 
-											for c in v:
-												if c not in custom_colors:
-													custom_colors.append(c)
+		if highlight_tracks:
+			self.parse_track(highlight_tracks, 'highlight')
 
-												cid = custom_colors.index(c)
-												cs.append('cc{}'.format(cid))
 
-											option(k, ','.join(cs))
+		with Tag('image'):
+			self.include('etc/image.conf')
 
-										else:
-											if v not in custom_colors:
-												custom_colors.append(v)
-
-											cid = custom_colors.index(v)
-											option(k, 'cc{}'.format(cid))
-
-									case _:
-										option(k, v)
-
-		with tag('image'):
-			include('etc/image.conf')
-
-		include('etc/colors_fonts_patterns.conf')
-		include('etc/housekeeping.conf')
+		self.include('etc/colors_fonts_patterns.conf')
+		self.include('etc/housekeeping.conf')
 
 		if custom_colors:
-			with tag('colors'):
+			with Tag('colors'):
 				for i, c in enumerate(custom_colors):
-					option('cc{}'.format(i), c)
+					self.option('cc{}'.format(i), c)
