232a3e5f94e7927b9d91a83c93db2fd37167960c
Lianming Du <adullb@qq.com>
2026-01-20 19:59:56 +0800

    update

--- package/build_mac_installer.sh
+++ package/build_mac_installer.sh
@@ -10,10 +10,27 @@ create-dmg \
 	--volicon "../src/icons/logo.icns" \
 	--background "../package/dmg_bg.png" \
 	--window-pos 640 360 \
-	--window-size 921 672 \
-	--icon-size 90 \
-	--icon "Circhart.app" 262 375 \
-	--app-drop-link 726 375 \
+	--window-size 690 503 \
+	--icon-size 120 \
+	--icon "Circhart.app" 196 281 \
+	--app-drop-link 544 281 \
 	--hdiutil-quiet \
 	"Circhart-v${version}-macos-${arch}.dmg" \
 	"Circhart.app"
+
+pkgbuild \
+	--identifier dulab.big.circhart \
+	--component \
+	Circhart.app \
+	Circhart-Component.pkg \
+	--install-location /Applications
+
+productbuild \
+	--synthesize \
+	--package Circhart-Component.pkg \
+	Circhart-distribution.xml
+
+productbuild \
+	--distribution Circhart-distribution.xml \
+	--package-path . \
+	Circhart-v${version}-macos-${arch}.pkg
