0133fa7ed6e6f535b2a3248f09e8fcb361a18b24
Lianming Du <adullb@qq.com>
2026-01-20 22:29:09 +0800

    Update build_linux_installer.sh

--- package/build_linux_installer.sh
+++ package/build_linux_installer.sh
@@ -79,8 +79,15 @@ else
 fi
 
 #build appimage
-wget --no-check-certificate --quiet https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
-chmod +x appimagetool-x86_64.AppImage
+sudo apt install coreutils binutils patchelf desktop-file-utils fakeroot fuse squashfs-tools strace util-linux zsync libgdk-pixbuf2.0-dev -y
+wget --no-check-certificate --quiet https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage -O $GITHUB_WORKSPACE/appimagetool
+chmod +x $GITHUB_WORKSPACE/appimagetool
+echo "APPIMAGETOOL_LOCATION=$GITHUB_WORKSPACE/appimagetool" >> $GITHUB_ENV
+
+pip install git+https://github.com/Frederic98/appimage-builder.git
+echo "APPIMAGEBUILDER_LOCATION=appimage-builder" >> $GITHUB_ENV
+
+echo "$GITHUB_WORKSPACE" >> $GITHUB_PATH
 
 cp circhart.desktop Circhart
 cp logo.svg Circhart/circhart.svg
