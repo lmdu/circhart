55e73da957577f185538017997ab2daacefee9dc
Lianming Du <adullb@qq.com>
2026-01-21 11:55:22 +0800

    update

--- package/build_linux_installer.sh
+++ package/build_linux_installer.sh
@@ -65,95 +65,172 @@ deb:
 EOF
 
 # copy logo file
-cp ../src/icons/logo.svg ./logo.svg
-cp ../src/icons/alogo.svg ./alogo.svg
+#cp ../src/icons/logo.svg ./logo.svg
+#cp ../src/icons/alogo.svg ./alogo.svg
 
-if [ "$packager" = "deb" ]
-then
-  ./nfpm pkg -t Circhart-v$version-linux.deb
-elif [ "$packager" = "rpm" ]
-then
-  ./nfpm pkg -t Circhart-v$version-linux.rpm
-else
-  echo $version
-fi
+#if [ "$packager" = "deb" ]
+#then
+#  ./nfpm pkg -t Circhart-v$version-linux.deb
+#elif [ "$packager" = "rpm" ]
+#then
+#  ./nfpm pkg -t Circhart-v$version-linux.rpm
+#else
+#  echo $version
+#fi
 
 #build appimage
 sudo apt install coreutils binutils patchelf desktop-file-utils fakeroot fuse squashfs-tools strace util-linux zsync libgdk-pixbuf2.0-dev -y
 wget --no-check-certificate --quiet https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage -O $GITHUB_WORKSPACE/appimagetool
 chmod +x $GITHUB_WORKSPACE/appimagetool
-echo "APPIMAGETOOL_LOCATION=$GITHUB_WORKSPACE/appimagetool" >> $GITHUB_ENV
 
 pip install git+https://github.com/Frederic98/appimage-builder.git
-echo "APPIMAGEBUILDER_LOCATION=appimage-builder" >> $GITHUB_ENV
-
 echo "$GITHUB_WORKSPACE" >> $GITHUB_PATH
 
-cp circhart.desktop Circhart
-cp logo.svg Circhart/circhart.svg
+cat > AppImageBuilder.yml <<EOF
+version: 1
 
-mkdir -p Circhart/usr/share/icons/hicolor/scalable/apps
-cp logo.svg Circhart/usr/share/icons/hicolor/scalable/apps/circhart.svg
+AppDir:
+  path: ./AppDir
+  
+  app_info:
+    id: dulab.big.circhart
+    name: Circhart
+    icon: circhart.svg
+    version: ${version}
+    exec: Circhart
+    exec_args: $@
+  
+  apt:
+    arch:
+      - amd64
+    
+    allow_unauthenticated: true
+      - sourceline: deb http://archive.ubuntu.com/ubuntu/ jammy main restricted universe multiverse
+      - sourceline: deb http://archive.ubuntu.com/ubuntu/ jammy-updates main restricted universe multiverse
+      - sourceline: deb http://security.ubuntu.com/ubuntu jammy-security main restricted universe multiverse
+    
+    include:
+      - xdg-desktop-portal-kde
+      - libgtk-3-0
+      - librsvg2-2
+      - librsvg2-common
+      - libgdk-pixbuf2.0-0
+      - libgdk-pixbuf2.0-bin
+      - libgdk-pixbuf2.0-common
+      - shared-mime-info
+      - gnome-icon-theme-symbolic
+      - hicolor-icon-theme
+    
+    exclude: []
+  
+  files:
+    include: []
+    
+    exclude:
+      - usr/share/man
+      - usr/share/doc/*/README.*
+      - usr/share/doc/*/changelog.*
+      - usr/share/doc/*/NEWS.*
+      - usr/share/doc/*/TODO.*
+      - usr/lib/x86_64-linux-gnu/libssl.so*
+  
+  runtime:
+    env:
+      APPDIR_LIBRARY_PATH: "$APPDIR:$APPDIR/runtime/compat/:$APPDIR/usr/lib/x86_64-linux-gnu:$APPDIR/lib/x86_64-linux-gnu:$APPDIR/usr/lib:$APPDIR/usr/lib/x86_64-linux-gnu/gdk-pixbuf-2.0/2.10.0/loaders"
+      LD_LIBRARY_PATH: "$APPDIR:$APPDIR/runtime/compat/:$APPDIR/usr/lib/x86_64-linux-gnu:$APPDIR/lib/x86_64-linux-gnu:$APPDIR/usr/lib:$APPDIR/usr/lib/x86_64-linux-gnu/gdk-pixbuf-2.0/2.10.0/loaders"
+      PYTHONPATH: "$APPDIR"
+      QT_QPA_PLATFORM: xcb
+      GDK_PIXBUF_MODULEDIR: $APPDIR/usr/lib/x86_64-linux-gnu/gdk-pixbuf-2.0/2.10.0/loaders
+      GDK_PIXBUF_MODULE_FILE: $APPDIR/usr/lib/x86_64-linux-gnu/gdk-pixbuf-2.0/2.10.0/loaders.cache
+    
+    path_mappings:
+      - /usr/share:$APPDIR/usr/share
 
-cat > Circhart/AppRun <<'EOF'
-#!/bin/bash
+  test:
+    fedora-30:
+      image: appimagecrafters/tests-env:fedora-30
+      command: ./AppRun
+      use_host_x: True
+    
+    debian-stable:
+      image: appimagecrafters/tests-env:debian-stable
+      command: ./AppRun
+      use_host_x: True
+    
+    archlinux-latest:
+      image: appimagecrafters/tests-env:archlinux-latest
+      command: ./AppRun
+      use_host_x: True
+    
+    centos-7:
+      image: appimagecrafters/tests-env:centos-7
+      command: ./AppRun
+      use_host_x: True
+    
+    ubuntu-xenial:
+      image: appimagecrafters/tests-env:ubuntu-xenial
+      command: ./AppRun
+      use_host_x: True
 
-appdir=$(dirname $0)
-
-exec "$appdir/Circhart" "$@"
-
+AppImage:
+  arch: x86_64
+  file_name: Circhart-v${version}-linux-x64.AppImage
+  update-information: guess
+  comp: gzip
 EOF
-chmod 755 Circhart/AppRun
 
-cat > Circhart/circhart.desktop <<EOF
-[Desktop Entry]
-Name=Circhart
-Comment=a user-friendly tool for drawing circos and snail plots
-Keywords=Circos;Snail;Genome
-Exec=Circhart %F
-Icon=circhart
-Terminal=false
-Type=Application
-Categories=Education
-MimeType=application/x-circ
-X-AppImage-Version=${version}
-EOF
+mv Circhart AppDir
+cp ../src/icons/logo.svg ./AppDir/circhart.svg
+#cp circhart.desktop Circhart
+#cp logo.svg Circhart/circhart.svg
 
-mkdir -p Circhart/usr/share/metainfo
-cat > Circhart/usr/share/metainfo/circhart.appdata.xml <<EOF
-<?xml version="1.0" encoding="UTF-8"?>
-<component type="desktop-application">
-<id>com.dulab.Circhart</id>
-<metadata_license>CC0-1.0</metadata_license>
-<project_license>MIT</project_license>
-<name>Circhart</name>
-<summary>Drawing Circos and Snail plots</summary>
-<description>
-  <p>Circhart is a user-friendly tool for drawing circos and snail plots</p>
-</description>
-<screenshots>
-  <screenshot type="default">
-    <caption>Circhart</caption>
-    <image>https://raw.githubusercontent.com/lmdu/circhart/main/src/icons/logo.svg</image>
-  </screenshot>
-</screenshots>
-<url type="homepage">https://github.com/lmdu/circhart</url>
-</component>
-EOF
+#mkdir -p Circhart/usr/share/icons/hicolor/scalable/apps
+#cp logo.svg Circhart/usr/share/icons/hicolor/scalable/apps/circhart.svg
 
-./appimagetool-x86_64.AppImage --appimage-extract-and-run Circhart Circhart-v$version-linux-${packager}.AppImage
-rm appimagetool-x86_64.AppImage
+#cat > Circhart/AppRun <<'EOF'
+##!/bin/bash
 
-#./nfpm pkg -t Dockey-v${version}-amd64.rpm
+#appdir=$(dirname $0)
 
-#if [ "$packager" = "deb" ]
-#then
-#  ./nfpm pkg -t Dockey-v${version}-amd64.deb
-#  #tar -czvf Dockey-v${version}-ubuntu.tar.gz Dockey
-#elif [ "$packager" = "rpm" ]
-#then
-#  ./nfpm pkg -t Dockey-v${version}-amd64.rpm
-#  #tar -czvf Dockey-v${version}-centos.tar.gz Dockey
-#else
-#  echo $version
-#fi
+#exec "$appdir/Circhart" "$@"
+
+#EOF
+#chmod 755 Circhart/AppRun
+
+#cat > Circhart/circhart.desktop <<EOF
+#[Desktop Entry]
+#Name=Circhart
+#Comment=a user-friendly tool for drawing circos and snail plots
+#Keywords=Circos;Snail;Genome
+#Exec=Circhart %F
+#Icon=circhart
+#Terminal=false
+#Type=Application
+#Categories=Education
+#MimeType=application/x-circ
+#X-AppImage-Version=${version}
+#EOF
+
+#mkdir -p Circhart/usr/share/metainfo
+#cat > Circhart/usr/share/metainfo/circhart.appdata.xml <<EOF
+#<?xml version="1.0" encoding="UTF-8"?>
+#<component type="desktop-application">
+#<id>com.dulab.Circhart</id>
+#<metadata_license>CC0-1.0</metadata_license>
+#<project_license>MIT</project_license>
+#<name>Circhart</name>
+#<summary>Drawing Circos and Snail plots</summary>
+#<description>
+#  <p>Circhart is a user-friendly tool for drawing circos and snail plots</p>
+#</description>
+#<screenshots>
+#  <screenshot type="default">
+#    <caption>Circhart</caption>
+#    <image>https://raw.githubusercontent.com/lmdu/circhart/main/src/icons/logo.svg</image>
+#  </screenshot>
+#</screenshots>
+#<url type="homepage">https://github.com/lmdu/circhart</url>
+#</component>
+#EOF
+
+appimage-builder --recipe AppImageBuilder.yml --skip-test
